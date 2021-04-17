2b1da66b9bf8bf75a0ea6c9d54079e95f34f16ed
KennyBruwier <79453343+KennyBruwier@users.noreply.github.com>
2021-04-17 14:17:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function lampAan()
+{
+    var pic = document.getElementById("bulb");
+    pic.src = "../../Images/pic_bulbon.gif";
+}
+
+function lampAf()
+{
+    var pic = document.getElementById("bulb");
+    pic.src = "../../Images/pic_bulboff.gif";
+    
+}
